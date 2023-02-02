9228b7a73d5ff7e7cc6f0f468ccf924905ae6139
Shinamide <olusholasalau@gmail.com>
2023-02-02 01:27:59 +0100

    This is an ammendment

---
+++ print.py
@@ -0,0 +1,3 @@
+Barca = 2
+Betis = 1
+print(f"Barcelona scored {Barca} and Betis scored {Betis} goals, tonight.. YES")
---
+++ printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+
+printf("Barcelona played against Betis on the %dnd of February, %d.\n The score line was Barca %d : %d Betis.\n", 2, 2023, 2, 1);
+    return(0);
+printf("Wonders shall never end\n");
+
+}
