6e7a8370462b295c76f4dcb89b558ef25ff458b2
Shinamide <olusholasalau@gmail.com>
2023-02-04 00:05:30 +0100

    Around 12:05

--- Bro_code/Draft1.c
+++ Bro_code/Draft1.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
 int main()
 
 {
+    
     printf("I love you Jesus\n");
     printf("I like food but I pray at all times\n");
+    
+    int x = 123;
+    float y = 123.2;
+    char grade = "A";
+    char check[] = "Boy, Girls";
+    printf("This is another reason why you need to score nothing more than %0.1f in the test\n", y);
+    printf("But you went ahead to score %d in the same test\n", x);
+    printf("Now your score remains a \"%c\" grade\n", grade);
+    printf("\t %s \n", check);
+
 
     return(0);
 }
